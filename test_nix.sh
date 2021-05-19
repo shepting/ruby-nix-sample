8f3724f4306873fc2c05e4af0c5c49fd68c76f1b
Steven Hepting <steven.hepting@airbnb.com>
2021-05-19 11:56:58 -0700

    Add better debugging for Bundler paths.

--- test_nix.sh
+++ test_nix.sh
@@ -3,10 +3,27 @@
 
 set -o errexit
 
+# For debugging
+DIM='\033[0;2m'
+NC='\033[0m' # No Color
+function debug () {
+    echo -e "${DIM}$1${NC}"
+}
+
+
 cd "$(dirname "$0")" # Run from the directory of this script
+
+# Log version of bundler used in Bundix
+echo
+echo "Inspect Bundix paths to see Bundler version"
+function path () { echo "$PATH" | tr : '\n'; }
+debug "$(cat "$(path | grep bundix)/bundix")"
+echo
 
 echo "Testing Ruby imports."
 ruby test_import.rb
+
+
 
 # Test bundix install with bundle locks
 # log "Testing Bundix generation"

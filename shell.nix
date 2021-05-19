fae85aa2dec0c475398c6582a9ac8910d1d94542
Steven Hepting <steven.hepting@airbnb.com>
2021-05-19 11:35:59 -0700

    Use a newer version of Bundler with Bundix.

--- shell.nix
+++ shell.nix
@@ -1,16 +1,27 @@
-with import<nixpkgs>{};
+
 let
-  gems = bundlerEnv {
+  pkgs = import <nixpkgs> { overlays = [
+    (self: super: {
+      bundler = self.callPackage ./bundler.nix {};
+    })
+    ];};
+  gems = pkgs.bundlerEnv {
     name = "bnb-gems";
     gemdir = ./.;
     copyGemFiles = true;
   };
 in
 
-mkShell {
+pkgs.mkShell {
   name = "managed-tooling-shell";
   buildInputs = [
+    # Uses new version of Bundler from overlay
+    pkgs.bundix
+
+    # Gems from Bundix
     gems
+
+    # Version of Ruby that matches the Gems
     gems.wrappedRuby
   ];
 
